9ed003cf3bdf7cff4205e8c534c9d2c98f2fa76a
Sebastian Öberg <sebastian.oberg@gmail.com>
2020-11-22 19:31:12 +0100

    Added project and Option type.

---
+++ src/ThrowAway.Tests/OptionTests.cs
@@ -0,0 +1,155 @@
+using System;
+using Xunit;
+using static ThrowAway.Option;
+
+namespace ThrowAway.Tests
+{
+    public class OptionTests
+    {
+        [Fact]
+        public void SomeValueType()
+        {
+            var three = Option.Some(3);
+
+            Assert.True(three.HasValue);
+            Assert.Equal(3, three.Value);
+        }
+
+        [Fact]
+        public void SomeStruct()
+        {
+            var now = DateTime.Now;
+            var date = Option.Some(now);
+
+            Assert.True(date.HasValue);
+            Assert.Equal(now, date.Value);
+        }
+
+        [Fact]
+        public void SomeReferenceType()
+        {
+            var @string = "123";
+            var test = Option.Some(@string);
+
+            Assert.True(test.HasValue);
+            Assert.Equal(@string, test.Value);
+        }
+
+        [Fact]
+        public void NoneValueType()
+        {
+            var test = Option<int>.None;
+
+            Assert.False(test.HasValue);
+            Assert.Throws<NoneException>(() => test.Value);
+        }
+
+        [Fact]
+        public void NoneTest()
+        {
+            var test = Option.None();
+
+            Assert.False(test.HasValue);
+            Assert.Throws<NoneException>(() => test.Value);
+        }
+
+        [Fact]
+        public void NoneStatic()
+        {
+            var test = None();
+
+            Assert.False(test.HasValue);
+            Assert.Throws<NoneException>(() => test.Value);
+        }
+
+        [Fact]
+        public void SomeStatic()
+        {
+            var test = Some("test");
+
+            Assert.True(test.HasValue);
+            Assert.Equal("test", test.Value);
+        }
+
+        [Fact]
+        public void SomeImplicit()
+        {
+            Assert.Equal("test", Some("test"));
+        }
+
+        [Fact]
+        public void SomeExplicit()
+        {
+            Assert.Equal("test", (string)Some("test"));
+        }
+
+        [Fact]
+        public void NoneImplicitFrom()
+        {
+            Assert.Throws<NoneException>(() => { string test = None<string>(); });
+        }
+
+        [Fact]
+        public void NoneImplicitTo()
+        {
+            Option<string> none = null;
+            Assert.False(none.HasValue);
+        }
+
+        [Fact]
+        public void NoneExplicitFrom()
+        {
+            Assert.Throws<NoneException>(() => (string)None<string>());
+        }
+
+        [Fact]
+        public void NoneExplicitTo()
+        {
+            var none = (Option<string>)null;
+            Assert.False(none.HasValue);
+        }
+
+        [Fact]
+        public void SomeNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Some<string>(null));
+        }
+
+        [Fact]
+        public void NoneInsideSome()
+        {
+            var none = (Option<string>)null;
+            var some = Some(none);
+            Assert.True(some.HasValue);
+            Assert.False(some.Value.HasValue);
+        }
+
+        [Fact]
+        public void NullableSome()
+        {
+            int? nullable = 3;
+            var some = Some(nullable);
+            Assert.True(some.HasValue);
+            Assert.Equal(3, some.Value);
+        }
+        
+        [Fact]
+        public void NullableNone()
+        {
+            int? nullable = null;
+            Assert.Throws<ArgumentNullException>(() => Some(nullable));
+        }
+
+        [Fact]
+        public void SomeToString()
+        {
+            Assert.Equal("3", Some(3).ToString());
+        }
+
+        [Fact]
+        public void NoneToString()
+        {
+            Assert.Equal("None", None().ToString());
+        }
+    }
+}
---
+++ src/ThrowAway/Option.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static ThrowAway.Helpers;
+
+namespace ThrowAway
+{
+    public static class Option
+    {
+        public static Option<T> Some<T>(T value) =>
+            Option<T>.Some(value);
+
+        public static Option<Nothing> None() =>
+            Option<Nothing>.None;
+
+        public static Option<T> None<T>() =>
+            Option<T>.None;
+    }
+
+    public readonly struct Option<T>
+    {
+        private readonly T value;
+        public readonly bool HasValue { get; }
+        public T Value => HasValue
+           ? value
+           : throw new NoneException();
+
+        private Option(T value, bool hasValue)
+        {
+            this.value = value;
+            this.HasValue = hasValue;
+        }
+
+        public static Option<T> Some(T value)
+        {
+            if (IsNull(value))
+                throw new ArgumentNullException(nameof(value), "'Some' cannot be called with a 'null' value");
+
+            return new Option<T>(value, true);
+        }
+
+        public static Option<T> None
+            => new Option<T>(default, false);
+
+        public static implicit operator T(Option<T> option) => option.Value;
+        public static implicit operator Option<T>(T value) => IsNull(value)
+            ? None
+            : Some(value);
+
+        public override string ToString() => HasValue
+            ? value.ToString()
+            : "None";
+    }
+}
